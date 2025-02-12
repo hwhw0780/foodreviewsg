0e434d46cd93cf41a661930ec2fb923226e27949
hwhw0780 <haowen0780@hotmail.com>
2025-02-12 15:52:05 +0800

    Fix S3 upload configuration to handle disabled ACLs

--- config/s3.js
+++ config/s3.js
@@ -9,16 +9,31 @@ const s3 = new AWS.S3({
 const bucket = process.env.AWS_BUCKET_NAME;
 
 async function uploadToS3(file, folder = '') {
+    const key = `${folder}/${Date.now()}-${file.originalname}`;
     const params = {
         Bucket: bucket,
-        Key: `${folder}/${Date.now()}-${file.originalname}`,
+        Key: key,
         Body: file.buffer,
-        ContentType: file.mimetype,
-        ACL: 'public-read'
+        ContentType: file.mimetype
     };
 
     try {
+        console.log('Uploading file to S3:', { bucket: params.Bucket, key: params.Key });
         const data = await s3.upload(params).promise();
+        
+        // Make the object public using putObjectAcl
+        try {
+            await s3.putObjectAcl({
+                Bucket: bucket,
+                Key: key,
+                ACL: 'public-read'
+            }).promise();
+        } catch (aclError) {
+            console.log('Note: Could not set ACL - this is expected if ACLs are disabled', aclError.message);
+            // Continue even if ACL setting fails - we'll rely on bucket policy
+        }
+
+        console.log('File uploaded successfully:', data.Location);
         return data.Location;
     } catch (error) {
         console.error('Error uploading to S3:', error);
@@ -33,7 +48,9 @@ async function deleteFromS3(key) {
     };
 
     try {
+        console.log('Deleting file from S3:', { bucket: params.Bucket, key: params.Key });
         await s3.deleteObject(params).promise();
+        console.log('File deleted successfully');
     } catch (error) {
         console.error('Error deleting from S3:', error);
         throw error;
